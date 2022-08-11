6c2ab734e2d3ef953dd14536ecaf5a46883bca70
unknown <fpcarlos@uerr.edu.br>
2022-08-11 16:12:11 -0400

    aplicação java spring boot

---
+++ apploja/src/main/java/br/edu/uerr/apploja/ApplojaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uerr.apploja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplojaApplication.class, args);
+	}
+
+}
---
+++ apploja/src/test/java/br/edu/uerr/apploja/ApplojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uerr.apploja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
